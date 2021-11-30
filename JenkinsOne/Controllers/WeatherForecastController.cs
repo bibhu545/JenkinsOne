867ea6edb5000e50a28f94feeeaadc1fbb88ee5b
bibhu545 <bibhuranjan.500@gmail.com>
2021-12-01 01:12:20 +0530

    test commit 4

--- JenkinsOne/Controllers/WeatherForecastController.cs
+++ JenkinsOne/Controllers/WeatherForecastController.cs
@@ -8,7 +8,8 @@ namespace JenkinsOne.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "hello"
+            // "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
